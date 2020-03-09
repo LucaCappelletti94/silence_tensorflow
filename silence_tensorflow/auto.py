440f6a6c02bddff49beffbb240d7131782896488
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-03-09 12:21:25 +0100

    Added auto sub module

---
+++ silence_tensorflow/auto.py
@@ -0,0 +1,3 @@
+from .silence_tensorflow import silence_tensorflow
+
+silence_tensorflow()
--- tests/test_silence_tensorflow.py
+++ tests/test_silence_tensorflow.py
@@ -3,3 +3,6 @@ from silence_tensorflow import silence_tensorflow
 def test_silence_tensorflow():
     """Check that everything runs."""
     silence_tensorflow()
+
+def test_auto_silence():
+    import silence_tensorflow.auto
